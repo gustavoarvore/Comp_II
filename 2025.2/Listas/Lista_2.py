9ca4949fd69e761d4d2695834183ab1000104a01
gustavo <gustavo.machado1p@gmail.com>
2025-08-26 12:29:22 -0300

    questão 2 da lista 2 de POO concluída; Dando in´cio à questão 3 da lista 2.

--- 2025.2/Listas/Lista_2.py
+++ 2025.2/Listas/Lista_2.py
@@ -1,55 +1,81 @@
 #Questão: Sistema de Temperatura
 
-class termometro:
-    def __init__(self, temperatura):
-        self.temperatura = float(temperatura)
+# class termometro:
+#     def __init__(self, temperatura):
+#         self.temperatura = float(temperatura)
 
-    def converter_fahrenheit(self):
-        return (self.temperatura * 9/5 + 32)
+#     def converter_fahrenheit(self):
+#         return (self.temperatura * 9/5 + 32)
 
-    def converter_kelvin(self):
-        return (self.temperatura + 273.15)
+#     def converter_kelvin(self):
+#         return (self.temperatura + 273.15)
 
-    def verificar_estado(self):
-        if self.temperatura < 0:
-            return "Sólido"
-        elif self.temperatura >= 0 and self.temperatura <= 100:
-            return "Líquido"
-        else:   
-            return "Gasoso"
+#     def verificar_estado(self):
+#         if self.temperatura < 0:
+#             return "Sólido"
+#         elif self.temperatura >= 0 and self.temperatura <= 100:
+#             return "Líquido"
+#         else:   
+#             return "Gasoso"
 
-termometro1 = termometro(input("Digite a Temperatura: "))
-print(f'A temperatura em Fahrenreit é {termometro1.converter_fahrenheit()}°F')
-print(f'A temperatura em Kelvin é {termometro1.converter_kelvin()}°K')
-print(f'O estado da água nessa temperatura é {termometro1.verificar_estado()}')
+# termometro1 = termometro(input("Digite a Temperatura: "))
+# print(f'A temperatura em Fahrenreit é {termometro1.converter_fahrenheit()}°F')
+# print(f'A temperatura em Kelvin é {termometro1.converter_kelvin()}°K')
+# print(f'O estado da água nessa temperatura é {termometro1.verificar_estado()}')
 
 #Questão: Estudante
 
-class estudante:
-    def __init__(self, nome, nota1, nota2, nota3):
-        self.nome = nome
-        self.nota1 = float(nota1)
-        self.nota2 = float(nota2)
-        self.nota3 = float(nota3)
+# class estudante:
+#     def __init__(self, nome, nota1, nota2, nota3, media=0):
+#         self.nome = nome
+#         self.nota1 = float(nota1)
+#         self.nota2 = float(nota2)
+#         self.nota3 = float(nota3)
+#         self.media = float(nota1+nota2+nota3)
 
-    def calcular_media(self):
-        return ((self.nota1 + self.nota2 + self.nota3) / 3)
+#     def calcular_media(self):
+#         return (self.media/3)
 
-    def verificar_aprovacao(self):
-        if calcular_media() >= 7:
-            print("Aprovado")
-        else:
-            print("Reprovado")
+#     def verificar_aprovacao(self):
+#         if self.calcular_media() >= 7:
+#             print(f"O(A) Aluno(a) {self.nome} foi Aprovado(a) com média {self.calcular_media()}")
+#         else:
+#             print(f"O(A) Aluno(a) {self.nome} foi Reprovado(a) com média {self.calcular_media()}")
             
-    def atualizar_notas(self):
-        while True: 
-            break
+#     def atualizar_notas(self, nota1_1, nota2_2, nota3_3):
+#         self.nota1_1 = nota1_1
+#         self.nota2_2 = nota2_2
+#         self.nota3_3 = nota3_3
+#         print(f'Notas Atualizadas: {self.nota1_1}, {self.nota2_2}, {self.nota3_3}\n')
     
-    def mostrar_dados():
-        return
+#     def mostrar_dados(self):
+#         print(f'Nome: {self.nome}, Notas: {self.nota1}, {self.nota2}, {self.nota3}, Média: {self.calcular_media()}')
 
-estudante1 = estudante(input("Digite o nome: "))
-print(f'A média do aluno é: {estudante1.calcular_media()}')
-print(f'Situação do(a) aluno(a): {estudante1.verificar_aprovacao()}')
-print(f'Atualização das notas: {estudante1.atualizar_notas()}')
-print(f'Dados do Estudante \n Nome: {self.nome}, Notas: {self.nota1}, {self.nota2}, {self.nota3}, Média: {}')
+# estudante1 = estudante("Gustavo", 2, 6.5 ,8)
+# print(f'A média do aluno é: {estudante1.calcular_media()}')
+# estudante1.verificar_aprovacao()
+# estudante1.mostrar_dados()
+# estudante1.atualizar_notas(2,4,5)
+
+# estudante2 = estudante("Sara", 7.2, 8.5, 6)
+# print(f'A média do aluno é: {estudante2.calcular_media()}')
+# estudante2.verificar_aprovacao()
+# estudante2.mostrar_dados()
+# estudante2.atualizar_notas(10,9,5)
+
+
+#Questão Reserva de Hotel
+
+class quartohotel:
+    def __init__(self, numero, tipo, ocupado):
+        self.numero = int(numero)
+        self.tipo = str(tipo)
+        self.ocupado = bool(ocupado)
+
+    def reservar(self):
+
+    def liberar(self):
+
+    def status(self):
+
+quartohotel_1 = quartohotel
