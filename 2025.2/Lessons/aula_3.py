12dbb43a661373b8d38b7ccbb2d66a60456ccfdc
Gustavo Machado <gustavo.machado1p@gmail.com>
2025-09-16 17:18:29 -0300

    Lições da aula 3 e utilização do espaço venv como kennel no notebook

--- 2025.2/Lessons/aula_3.py
+++ 2025.2/Lessons/aula_3.py
@@ -11,7 +11,7 @@ class livro:
         if self.num_paginas > 500:
             return f"O livro {self.titulo} eh longo"
         else:
-            f'O livro {self.titulo} não eh tao longo assim'
+            return f'O livro {self.titulo} não eh tao longo assim'
     
 livro1 = livro("1984", "Jorge Orwell", 380)
 print(livro1.detalhes())
@@ -20,7 +20,7 @@ livro2 = livro("Don Quixote", "Miguel de Cervantes", 863)
 print(livro2.eh_longo())
 
 
-class ebook:
+class ebook(livro):
     def __init__(self, titulo, autor, num_paginas, formato):
         super().__init__(titulo, autor, num_paginas)
         self.formato = formato
@@ -32,3 +32,19 @@ ebook1 = ebook("Python para iniciantes", "João Silva", 250, "PDF")
 print(ebook1.detalhes_ebook())
 print(ebook1.eh_longo())
 
+class audiobook(livro):
+    def __init__(self, titulo, autor, num_paginas, duracao):
+        super().__init__(titulo, autor, num_paginas)
+        self.duracao = duracao
+        
+    def detalhes_audiobook(self):
+        return f'O livro {self.titulo}, escrito por {self.autor}, com a duracao de {self.duracao} minutos contem {self.num_paginas}.'
+        
+    def tempo_estimado_leitura(self, velocidade=1.0):
+        return self.duracao/velocidade
+        
+audiobook1 = audiobook("O Senhor dos Aneis", "J.R.R.", 1000, 1200)
+print(audiobook1.detalhes_audiobook())
+print(audiobook1.tempo_estimado_leitura(1.0))
+print(audiobook1.tempo_estimado_leitura(1.5))
+print(audiobook1.tempo_estimado_leitura(2.0))
