d0b8ffd53501af7a6d329f9e026ebf1e03987c7c
gustavo <gustavo.machado1p@gmail.com>
2025-09-18 09:37:56 -0300

    Término da aula 5 de comp II, onde foi introduzido Herança Múltipla e um pouco dos Métodos Mágicos.

---
+++ 2025.2/Lessons/aula_4.py
@@ -0,0 +1,44 @@
+#Herança Múltipla
+
+class tratamentotermico:
+    # classe base I
+    def __init__(self, temperatura:float):
+        self.temperatura = temperatura
+    
+    def tratar(self):
+        return f'Executando tratamento térmico a {self.temperatura} graus Celsius'
+
+class controlequalidade:
+    # classe base II
+    def __init__(self, metodo:str, frequencia:int):
+        self.metodo = metodo
+        self.frequencia = frequencia
+
+    def controlar(self):
+        return f'Controle de qualidade usando o método {self.metodo}'
+               f'{self.frequencia} vezes por dia ela é executada.'
+
+class processometalurgico(tratamentotermico, controlequalidade):
+    def __init__(self, temperatura:float, metodo:str, frequencia:int):
+        tratamentotermico.__init__(self, temperatura)
+        controlequalidade.__init__(self, metodo, frequencia)
+
+processo1 = processometalurgico(850, "Ultrassonografia", 3)
+processo1.tratar()
+processo1.controlar()
+
+# --------------------------------------------------------------------------------
+
+class numero:
+    # classe que representa um numero float
+    def __init__(self, num:float):
+        self.__num = num
+
+    def __add__(self, other):
+        print(f'Retorna {self.__num} + {other.__num}')
+        return self.__num + other.__num
+
+num_1 = numero(4.5)
+num_2 = numero(1.7)
+
+print(num_1 + num_2)
