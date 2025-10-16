5202a589453063e702fc7845a343150c8c70af34
gustavo <gustavo.machado1p@gmail.com>
2025-10-16 15:36:12 -0300

    Finalização do conteúdo da aula 6, com exceção dos 2 exercicios não finalizados.

--- 2025.2/Lessons/aula_6.py
+++ 2025.2/Lessons/aula_6.py
@@ -1,5 +1,42 @@
+# Para escrever dentro do arquivo
 arq = open('arq_teste.txt', 'w')
-
 arq.write('hello world')
-
 arq.close()
+
+# Apenas para leitura do arquivo
+arq = open('arq_teste.txt', 'r')
+info = arq.read()
+print(info)
+arq.close()
+
+# Lendo o arquivo linha por linha
+arq = open('arq_teste.txt', 'w')
+arq.write('Oi /nVc q ta lndo /nnão fode')
+arq = open('arq_teste.txt', 'r')
+for linha in arq:
+    print(linha)
+arq.close()
+
+# Retornando ao inicio de um arquivo
+arq = open('arq_teste.txt', 'w')
+arq.write('Oi /nVc q ta lndo /nnão fode')
+arq = open('arq_teste.txt', 'r')
+for linha in arq:
+    print(linha)
+arq.seek(0)
+arq.close()
+
+# FileNotFoundError
+try:
+    arq = open('arq_teste.txt', 'r')
+    conteudo = arq.read()
+except FileNotFoundError:
+    print('Erro: O arquivo não foi encontrado!')
+
+# IOError
+try:
+    arq = open('arq_teste.txt', 'w')
+    conteudo = arq.read()
+except IOError:
+    print('Erro: Deu ruim!')
+
