85c66a9b953d97a2fb4495e834c98194632d1346
gustavo <gustavo.machado1p@gmail.com>
2025-10-23 15:49:35 -0300

    Lista 6

--- 2025.2/Listas/Lista_6.py
+++ 2025.2/Listas/Lista_6.py
@@ -1,12 +1,13 @@
 # Exercicio 1
 
+arq = open('nomes.txt', 'w')
 for i in range(5):
-    arq = open('nomes.txt', 'w')
-    arq.write(input('Digite um nome: '))
-    arq = open('nomes.txt', 'r')
-    for linha in arq:
-        print(linha)
-    arq.close()
+    nome = input('Digite um nome: ')
+    arq.write(nome + '\n')  
+arq = open('nomes.txt', 'r')
+for linha in arq:
+    print(linha)  
+arq.close()
 
 
 # Questão 2
@@ -27,10 +28,29 @@ except PermissionError:
 arq = open('texto.txt', 'w')
 arq.write('Seu Zé: sim, fui...fui eu quem comeu o bolo - disse Zé com um tom de superioridade \nJoana: Ah não, Zé - com os olhos lacrimejando - eu guardei pra mim da festa da Tia Lucia \nQuando derrepente \nPOW!!! \nSurge Dona Eldineia com um dos pés sem o chinelo Havaiana \nDona Eldineia: POIS VOCÊ TRATE DE COMPRAR UM BOLO PRA ELA AGORA! - aos berros \nSeu Zé sentiu um frio nas costas e saiu correndo até a padaria da esquina.')
 arq = open('texto.txt', 'r')
-for linha in arq:
-    print(linha)
+conteudo = arq.read()
+palavras = conteudo.split()
+
+num_linhas = len(linhas)
+num_palavras = sum(len(conteudo.split()) for linha in arq)
+num_caracteres = len(conteudo)
+
+print(f'numero total de linhas: {num_linhas}')
+print(f'numero total de palavras: {num_palavras}')
+print(f'numero total de caracteres: {num_caracteres}')
 arq.close()
 
 
 # Questão 4
 
+try:
+    arq = open('', 'w')
+    arq.write('seila')
+    arq = open('', 'r')
+    for linha in arq:
+        print(linha)
+    arq.close()
+except FileNotFoundError:
+    print('Erro: Arquivo não encontrado')
+except ValueError:
+    print('Erro: Linha não pode ser convertida para número')
