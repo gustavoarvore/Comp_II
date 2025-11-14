1858f68d96cbc26cddc357acee2bc79f985afe93
Gustavo Machado <gustavo.machado1p@gmail.com>
2025-11-13 23:01:35 -0300

    inicio dos exercicios da aula 8. É divertido.

---
+++ 2025.2/Lessons/aula_8.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# fig, ax = plt.subplots()
+
+# ax.plot([1,2,3,4], [10,20,25,30])
+
+# ax.set_title('Gráfico Simples')
+# ax.set_xlabel('Eixo X')
+# ax.set_ylabel('Eixo Y')
+
+# plt.show()
+
+# -------------------------------------------------------------
+
+# fig, ax = plt.subplots()
+
+# x = np.linspace(0, 10, 200)
+# y = np.sin(x)
+
+# ax.plot(x, y, 'b-', linewidth=5)
+# ax.set_title('Gráfico Do Seno')
+# ax.set_xlabel(' Eixo X')
+# ax.set_ylabel(' Eixo Y')
+
+# plt.show()
+
+# -------------------------------------------------------------
+
